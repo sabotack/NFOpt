51d64d3f2757c830419921bf93aa5528fe34ee4a
Emil Monrad Laursen <emla21@student.aau.dk>
2024-04-03 17:12:01 +0200

    Trying to increase shareholder value by utilizing paths instead of links in the calculations, shoutout to the heavy hitters of America

--- paths.py
+++ paths.py
@@ -5,9 +5,10 @@ import numpy as np
 
 TRAFFIC_HOUR_0 = 699638
 PATHS_HOUR_0 = 2349348
-traffic_per_link = {}
+traffic_per_path = {}
 graph_hash = {}
 ratio_hash = {}
+capacity_per_path = {}
 path_names = ['timeStamp', 'pathStart', 'pathEnd', 'linksInPath']
 traffic_names = ['timeStamp', 'pathStart', 'pathEnd', 'traffic']
 total_time_convert = 0
@@ -16,7 +17,8 @@ total_time_traffic = 0
 total_time_read = []
 
 LR = 0.3
-ITERATIONS = 3
+ITERATIONS = 1
+OFFSET = -0.8
 
 # Functions -------------------------------------------------------------------
 def numRouters_to_pathArr(num_routers, path_str):
@@ -40,7 +42,8 @@ def pathStr_to_pathArr(path_str):
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
-    
+
+
 # Read the link capacities and store them in a hash table ---------------------
 start_r = time.time()
 links = pd.read_csv('dataset-week/links.csv.gz', compression='gzip')
@@ -87,17 +90,29 @@ for i in paths.index:
             start_t = time.time()
             ratio_per_path = 1/len(cur_paths)
             graph_hash[cur_flow_name] = cur_paths
+            flow_weights = []
+            flow_traffic = []
+            path_capacity = []
+
             for path in cur_paths:
-                for j in range(len(path)-1):
-                    link = path[j] + path[j+1]
-                    if link in traffic_per_link:
-                        traffic_per_link[link] += traffic[cur_flow_name] * ratio_per_path
-                    else:
-                        traffic_per_link[link] = traffic[cur_flow_name] * ratio_per_path
-                        ratio_hash[link] = ratio_per_path
+                capacity = 0
+                # Add the weights for each path
+                flow_weights.append(ratio_per_path)
+                # Calculate the amount of traffic that passes through each path
+                # times len(path)-1 is equal to the number of links
+                flow_traffic.append(traffic[cur_flow_name] * ratio_per_path)
+                for i in range(len(path) - 1):
+                    link = path[i] + path[i + 1]
+                    if link in links_hash:
+                        capacity += links_hash[link]
+                path_capacity.append(capacity)
+
+            ratio_hash[cur_flow_name] = flow_weights
+            traffic_per_path[cur_flow_name] = flow_traffic
+            capacity_per_path[cur_flow_name] = path_capacity
             end_t = time.time()
             total_time_traffic += end_t - start_t
-        
+
         # Reset the current flow and add the next path to the array --------
         cur_flow_name = name
         cur_paths = []
@@ -107,14 +122,16 @@ end_p = time.time()
 total_time_paths = end_p - start_p
 print('Done calculating traffic per link')
 
-# Calculate link utilization per link -----------------------------------------
-for link in traffic_per_link:
-    if link in links_hash:
-        traffic_per_link[link] = traffic_per_link[link] / links_hash[link] * 100
-        if traffic_per_link[link] > 10:
-            print(f'Link: {link} has {int(traffic_per_link[link])}% link util')
-    else:
-        traffic_per_link[link] = 0
+
+# Calculate path utilization -----------------------------------------
+def calc_path_util(traffic_per_path, capacity_per_path):
+    for flow in traffic_per_path:
+        print(f"\nFlow: {flow}")
+        for path_traffic in traffic_per_path[flow]:
+            print(f"Traffic: {path_traffic}")
+        for path_capacity in capacity_per_path[flow]:
+            print(f"Capacity: {path_capacity}")
+
 
 # Print the times -------------------------------------------------------------
 print(f'Time spent reading: {total_time_read[0]} seconds')
@@ -124,27 +141,37 @@ print(f'Total time spent converting string: {int(total_time_convert)} seconds')
 print(f'Total time spent calculating paths: {int(total_time_paths)} seconds')
 print(f'Total time spent calculating traffic: {int(total_time_traffic)} seconds')
 
+# Run path util ---
+calc_path_util(traffic_per_path, capacity_per_path)
+
 # Sort the traffic per link hash table on procent util ------------------------
-traffic_per_link = dict(sorted(traffic_per_link.items(), key=lambda item: item[1], reverse=True))
+# traffic_per_link = dict(sorted(traffic_per_link.items(), key=lambda item: item[1], reverse=True))
 
 # Make a graph of the traffic per link ----------------------------------------
-x = list(traffic_per_link.keys())
-y = list(traffic_per_link.values())
-plt.plot(x, y)
-plt.xlabel('Links')
-plt.ylabel('Utilization (%)')
-plt.xticks(rotation=60)
-plt.xticks(range(0, len(x), 300))
-plt.show()
+# x = list(traffic_per_link.keys())
+# y = list(traffic_per_link.values())
+# plt.plot(x, y)
+# plt.xlabel('Links')
+# plt.ylabel('Utilization (%)')
+# plt.xticks(rotation=60)
+# plt.xticks(range(0, len(x), 300))
+# plt.show()
 
 # Backpropagation on the graphs to update weights -----------------------------
+print(f"\nInitial ratios:\n{list(ratio_hash.items())[:30]}")
 for i in range(ITERATIONS):
     for flow in graph_hash:
+        path_weight = 0
+        flow_weights = []
         for path in graph_hash[flow]:
-            for j in range(len(path)-1):
-                link = path[j] + path[j+1]
-                ratio = ratio_hash[link]
-                traffic = traffic_per_link[link]
+            # Update the weight of the link
+            flow_weights.append(
+                sigmoid(
+                    ratio_hash[flow][path_weight]
+                    + (sigmoid(traffic_per_path) + OFFSET) * LR
+                )
+            )
+            path_weight += 1
 
-                # Update the weight of the link
-                ratio_hash[link] = # CRAZY MACHINE LEARNING CALCULATION
+        ratio_hash[flow] = flow_weights
+print(f"\nNew ratios:\n{list(ratio_hash.items())[:30]}")
